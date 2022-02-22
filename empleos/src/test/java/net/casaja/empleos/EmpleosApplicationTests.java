ef743517c26185740b3bcf9b34278aa6a9311448
javiers.castillo <casaja@gmail.com>
2022-02-22 21:16:22 +0100

    Initial commit of project

---
+++ empleos/src/test/java/net/casaja/empleos/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.casaja.empleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
